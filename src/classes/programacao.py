4b578c4cee365c61c5588fd4366eb9700be90778
Raian Satyro <raiansatyro45@gmail.com>
2021-09-21 21:49:59 -0300

    Feito o construtor da classe programacao

---
+++ src/classes/programacao.py
@@ -0,0 +1,10 @@
+class Programacao:
+    def __init__(self, id, nome, duracao, genero, idioma, legenda, ano, avaliacao):
+        self.id = id
+        self.nome = nome
+        self.duracao = duracao
+        self. genero = genero
+        self.idioma = idioma
+        self. legenda = legenda
+        self.ano = ano
+        self.avaliacao = avaliacao
