01b3e12dffae40ecd0d9a27926cc01dd64c81462
Thiago Feijo <tfeijo@gmail.com>
2021-10-05 21:22:19 -0300

    Teste: Uso inicial das subclasses

---
+++ principal.py
@@ -0,0 +1,8 @@
+from src.classes.filme import Filme
+from src.classes.serie import Serie
+
+novo_filme = Filme(1, "John Wick", "Ação", "Inglês", "Português", 2020, 93)
+print(novo_filme)
+
+nova_serie = Serie(1, "Breaking Bad", "Ação", "Inglês", "Espanhol", 2011, 53)
+print(nova_serie)
