cb3d51e0a01443d52b6f5cb6853a5f1335ba5f30
Thiago Feijo <tfeijo@gmail.com>
2021-10-07 21:32:38 -0300

    Initial flask

--- principal.py
+++ principal.py
@@ -2,7 +2,15 @@ from src.classes.filme import Filme
 from src.classes.serie import Serie
 from src.dao.serie_dao import SerieDAO
 from src.bd.bd_connection import bd
+from flask import Flask, jsonify, request
 
-nova_serie = Serie(1, "Breaking Bad", "Ação", "Inglês", "Espanhol", 2011, 53)
-nova_serie_dao = SerieDAO(nova_serie)
-bd.insert_in_BD(nova_serie_dao._programacao_json)
+
+app = Flask(__name__)
+@app.route('/', methods=['GET']) 
+def home():
+    return '''<html>'
+        <center>Eu estou online! - Hello World<center/>
+    </html>'''
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='3001')
